f0e22dfa3f842dc191e3f8ef9858c23478ef6e59
harsh <harshbali374@gmail.com>
2025-08-14 13:42:49 +0530

    increased the limit of the app

--- src/app.js
+++ src/app.js
@@ -24,10 +24,10 @@ if (config.env !== 'test') {
 app.use(helmet());
 
 // parse json request body
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // parse urlencoded request body
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // gzip compression
 app.use(compression());
